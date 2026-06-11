92860b806b30b4a8b07c8eb5197df61ff45d642a
Condor2026 <268162817+Condor2026@users.noreply.github.com>
2026-06-12 01:00:05 +0200

    Add SPDX license and copyright information
    Added licensing information and copyright notice.

--- Diabolic_It.py
+++ Diabolic_It.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-FileCopyrightText: 2026 Condor2026 / SpectrumSecurity
+#
+# Diabolic_It - Ireland Crime Intelligence Platform
+# Copyright (C) 2026 Condor2026
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
 """
 🔥 DIABOLIC ITALIA v6.0 - OSINT ANALYTICS PLATFORM
 Monitorización de criminalidad en Italia (versión estable)
